c68530ad960330adb88221aeddf280bd538ade42
Wonderland23333 <109506683+Wonderland23333@users.noreply.github.com>
2022-07-18 18:09:30 +0800

    Update vec2.rs

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,12 +7,14 @@
 // Execute the command `rustlings hint vec2` if you need
 // hints.
 
-// I AM NOT DONE
+
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for i in v.iter_mut() {
+    for i in v.iter_mut() {//返回可修改可重入迭代器，返回类型&mut T
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
+        *i *= 2;
+        
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
