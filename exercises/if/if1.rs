ae5f540a0c4c2c32a148dd75c49f1895b3e7f12e
Wonderland23333 <109506683+Wonderland23333@users.noreply.github.com>
2022-07-18 18:25:58 +0800

    Update if1.rs

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,38 +1,32 @@
-// if2.rs
-
-// Step 1: Make me compile!
-// Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
-// Execute the command `rustlings hint if2` if you want a hint :)
+// if1.rs
 
 
 
-pub fn fizz_if_foo(fizzish: &str) -> &str {
-    if fizzish == "fizz" {
-        "foo"
-    } else if fizzish == "fuzz"{
-        "bar"
+pub fn bigger(a: i32, b: i32) -> i32 {
+    // Complete this function to return the bigger number!
+    // Do not use:
+    // - another function call
+    // - additional variables
+    // Execute `rustlings hint if1` for hints
+    if a>b{
+        a
     }else{
-        "baz"
+        b
     }
 }
 
-// No test changes needed!
+// Don't mind this for now :)
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn foo_for_fizz() {
-        assert_eq!(fizz_if_foo("fizz"), "foo")
+    fn ten_is_bigger_than_eight() {
+        assert_eq!(10, bigger(10, 8));
     }
 
     #[test]
-    fn bar_for_fuzz() {
-        assert_eq!(fizz_if_foo("fuzz"), "bar")
-    }
-
-    #[test]
-    fn default_to_baz() {
-        assert_eq!(fizz_if_foo("literally anything"), "baz")
+    fn fortytwo_is_bigger_than_thirtytwo() {
+        assert_eq!(42, bigger(32, 42));
     }
 }
