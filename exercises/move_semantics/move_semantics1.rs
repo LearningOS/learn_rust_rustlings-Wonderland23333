9c4180a7611ba48fd0d60b6bfee81544a34aea09
Wonderland23333 <109506683+Wonderland23333@users.noreply.github.com>
2022-07-18 18:35:15 +0800

    Update move_semantics1.rs

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,14 +1,17 @@
-// move_semantics1.rs
-// Make me compile! Execute `rustlings hint move_semantics1` for hints :)
+// move_semantics2.rs
+// Make me compile without changing line 13 or moving line 10!
+// Execute `rustlings hint move_semantics2` for hints :)
 
 
 
 fn main() {
-    let vec0 = Vec::new();
+    
+    let vec0 = Vec::<i32>::new();
+    let mut vec1 = fill_vec(vec0.to_vec());
 
-    let mut vec1 = fill_vec(vec0);
-
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
     vec1.push(88);
 
